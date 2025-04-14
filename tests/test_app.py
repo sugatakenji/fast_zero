f92cbaa13663e9edb6a38b44554350ff9a97b987
leopoldo <sugatakenji@gmail.com>
2025-04-14 09:29:06 -0300

    Execícios da aula 03

--- tests/test_app.py
+++ tests/test_app.py
@@ -41,6 +41,22 @@ def test_read_users(client):
     }
 
 
+def test_read_user_by_id(client):
+    response = client.get('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_user_by_id_not_found(client):
+    response = client.get('/users/0')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
 def test_update_user(client):
     reponse = client.put(
         '/users/1',
