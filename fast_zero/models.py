58bde6ba27319c5ff8c20f14ef80cb742ce4096f
leopoldo <sugatakenji@gmail.com>
2025-04-15 10:58:04 -0300

    Exercício 4, migrations do banco de dados.

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -17,3 +17,6 @@ class User:
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
+    update_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), onupdate=func.now()
+    )
